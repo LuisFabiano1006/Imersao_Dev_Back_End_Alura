c5f4bcd18a67ca49515a1cff869430a9a189fa5d
Luis Fabiano Gomes <luidibaricheli000@gmail.com>
2024-11-22 16:20:55 -0300

    Aula 5: Configuração de API e Integração com Gemini

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 import express from "express";
-// Importa o framework Express, que será utilizado para criar o servidor web.
 import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-// Cria uma instância do Express, que será o núcleo do nosso servidor.
-routes(app);
+app.use(express.static("uploads"))
+routes(app)
 
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-    // Inicia o servidor na porta 3000 e exibe uma mensagem no console quando o servidor estiver pronto.
 });
